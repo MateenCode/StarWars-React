251013301d5d866252ba21b8b9b16c652ad160c0
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-07-24 14:08:29 -0500

    React

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css'
+export default class Header extends Component {
+  render(){
+    return (
+      <div className="header">
+
+      <h1>Star Wars</h1>
+      <hr></hr>
+      <p>The Vehicles of Stars Wars</p>
+      </div>
+
+    )
+  }
+}
